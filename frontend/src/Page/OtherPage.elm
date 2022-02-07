dd72b3e8c15d7abeed6afb59f62ca207f8b8beae
Max Kaemmerer <max.kaemmerer@wolters-rundreisen.de>
2022-02-07 17:53:42 +0100

    format elm

--- frontend/src/Page/OtherPage.elm
+++ frontend/src/Page/OtherPage.elm
@@ -1,10 +1,9 @@
 module Page.OtherPage exposing (view)
 
-import Accessibility exposing (Html, div, text)
-import Accessibility exposing (decorativeImg)
+import Accessibility exposing (Html, decorativeImg, div, text)
 import Html.Attributes exposing (src)
 
 
 view : Html msg
 view =
-    div [] [ text "Other Page Content", decorativeImg [ src "/img/florian-olivo-4hbJ-eymZ1o-unsplash.webp"] ]
+    div [] [ text "Other Page Content", decorativeImg [ src "/img/florian-olivo-4hbJ-eymZ1o-unsplash.webp" ] ]
